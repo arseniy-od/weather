b592d96868db869df0900c7b9e510b93157d52b7
Vasiliy <email@domain.com>
2022-11-25 18:42:29 +0200

    changed coords type to namedtuple

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import geocoder
 import requests
+from collections import namedtuple
+
+Coords = namedtuple('Coords', 'latitude longitude')
 
 
-def get_gps_coordinates():
+def get_gps_coordinates() -> Coords:
+    """Returns current coordinates using ip"""
     g = geocoder.ip('me')
-    return g.latlng
+    try:
+        lat, lng = g.latlng
+    except TypeError as e:
+        print("Cannot get coordinates from ip. Check your Internet connection")
+        raise e
+    return Coords(lat, lng)
 
 
 def get_weather(coordinates):
-    if coordinates is None:
-        raise Exception("Can't get coordinates")
-    lt, ln = coordinates
+    latitude, longitude = coordinates.latitude, coordinates.longitude
     url_api = "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current_weather=true"
-    weather_dict = requests.get(url_api.format(lt, ln)).json()
+    weather_dict = requests.get(url_api.format(latitude, longitude)).json()
     return weather_dict
 
 
