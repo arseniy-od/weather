d5b3f0a9d2e48bd753ab9df089c499b8b1507b8a
Vasiliy <email@domain.com>
2022-11-28 13:42:58 +0200

    Added config file

--- coordinates.py
+++ coordinates.py
@@ -1,4 +1,5 @@
 import geocoder
+import config
 from typing import NamedTuple
 from exceptions import CantGetCoordinates
 
@@ -16,4 +17,13 @@ def get_coordinates() -> Coords:
         raise CantGetCoordinates("Cannot get coordinates from ip. Check your Internet connection")
     lat, lng = g.latlng
     city = g.city
-    return Coords(lat, lng, city)
+    coords = Coords(lat, lng, city)
+    if config.ROUND_COORDINATES:
+        coords = _round_coords(coords)
+    return coords
+
+
+def _round_coords(coords: Coords) -> Coords:
+    """Rounds latitude and longitude to 1 decimal point"""
+    coords = Coords(round(coords.latitude, 1), round(coords.longitude, 1), coords.city)
+    return coords
